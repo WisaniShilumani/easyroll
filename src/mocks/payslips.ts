6c3fdcdd67bc5dde50664e9c223ca87533e32a45
Wisani <wisani@nonacreative.com>
2019-02-04 10:52:25 +0200

    fix: update mocks

--- src/mocks/payslips.ts
+++ src/mocks/payslips.ts
@@ -24,6 +24,7 @@ for (let i = 1; i < 2000; i++) {
     employeeId: faker.random.number({min: 0, max: 20}),
     paymentDate: faker.date.between('2015-01-01', '2019-01-01'),
     annualIncomeSnapshot: faker.random.number({min: 10000, max: 200000}),
+    pensionContributionSnapshot: faker.random.number({min: 0, max: 50}),
   }
 
   payslips.push(payslip)
